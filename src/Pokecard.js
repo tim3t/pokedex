7f27f5b7d7e0366ae628740a03fd3c0edd976c42
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-11-20 18:29:03 -0500

    Working pokedex, unstyled

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Pokecard = (props) => {
 	return (
-		<React.Fragment>
-			<h1>{props.name}</h1>
-			<p>{props.image}</p>
-			<p>{props.type}</p>
-		</React.Fragment>
+		<div className='Pokecard'>
+			<div>{props.name}</div>
+			<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} />
+			<div>Type: {props.type}</div>
+			<div>Experience: {props.exp}</div>
+		</div>
 	);
 };
 
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,8 +1,15 @@
 import Pokecard from './Pokecard';
 import defaultProps from './defaultProps';
 
-const Pokedex = () => {
-	return <Pokecard name='Charmander' image='imgLink' type='Lizard' />;
+const Pokedex = (props) => {
+	return (
+		<div className='Pokedex'>
+			<h1 className='Pokedex-Title'>Pokedex</h1>
+			<div className='Pokedex-Cards'>
+				{defaultProps.map((p) => <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />)}
+			</div>
+		</div>
+	);
 };
 
 export default Pokedex;
